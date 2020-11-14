9f275864b11e6a5cbea744d1b8a429543e955ba1
Batuhan Kadik <b.kadik@szutech.com>
2020-11-14 21:34:37 +0300

    package json, pattern

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'movies',
-    templateUrl:'movies.component.html'
+    templateUrl:'movies.component.html',
+    styles:[
+        'h2 { color :blue;}'
+    ]
 })
 export class MoviesComponent{
 
