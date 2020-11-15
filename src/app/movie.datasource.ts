c70d843e835492092ce4684ae8d192864d7d197e
Batuhan Kadik <b.kadik@szutech.com>
2020-11-15 16:33:50 +0300

    dashboard

--- src/app/movie.datasource.ts
+++ src/app/movie.datasource.ts
@@ -1,9 +1,9 @@
 import {Movie} from './movie'
 
 export const Movies : Movie[] = [
-    {id:11,name:"The Green Mile"},
-    {id:22,name:"The Shawhank Redemption"},
-    {id:33,name:"Sunshine of the Spotless Mind"},
-    {id:44,name:"Interstellar"}
+    {id:11,name:"The Green Mile",description:"TEST 1",imageUrl:"1.jpg"},
+    {id:22,name:"The Shawhank Redemption",description:"TEST 2",imageUrl:"2.jpg"},
+    {id:33,name:"Sunshine of the Spotless Mind",description:"TEST 3",imageUrl:"3.jpg"},
+    {id:44,name:"Interstellar",description:"TEST 4",imageUrl:"4.jpg"}
 ]
     
