4936cd6d911190a6f9308f9bf3287f9dbfe5ecf0
Batuhan Kadik <b.kadik@szutech.com>
2020-11-15 17:52:39 +0300

    update

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Movie } from './movie';
-import { Movies } from './movie.datasource';
 import { Observable, of } from 'rxjs';
 import { LoggingService } from './logging.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MovieService {
-
-  private apiMoviesUrl = 'api/movies'
+  private apiMoviesUrl = 'api/movies';
 
   constructor(
     private loggingService: LoggingService,
@@ -24,6 +22,13 @@ export class MovieService {
 
   getMovie(id: number): Observable<Movie> {
     this.loggingService.add('MovieService: get detail by id=' + id);
-    return this.http.get<Movie>(this.apiMoviesUrl+'/'+id);
+    return this.http.get<Movie>(this.apiMoviesUrl + '/' + id);
+  }
+
+  update(movie: Movie): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    }
+    return this.http.put(this.apiMoviesUrl, movie,httpOptions);
   }
 }
